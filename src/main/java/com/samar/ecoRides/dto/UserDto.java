6ae640e28b5df617849305672d6988c0236b37a7
samar <samarmod251@gmail.com>
2025-09-23 21:04:11 +0530

    save

--- src/main/java/com/samar/ecoRides/dto/UserDto.java
+++ src/main/java/com/samar/ecoRides/dto/UserDto.java
@@ -1,13 +1,11 @@
 package com.samar.ecoRides.dto;
 
 
-import com.samar.ecoRides.model.Ride;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Data
 @NoArgsConstructor
